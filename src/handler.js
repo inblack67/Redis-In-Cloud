7031690c982a21ab0fd7a7e87b1bff7cc59d2059
Aman Bhardwaj <devproddeploy@gmail.com>
2020-10-15 18:22:21 +0530

    prod

--- src/handler.js
+++ src/handler.js
@@ -1,14 +1,14 @@
 import nextConnect from 'next-connect';
 import 'colors';
 import redis from 'redis';
+import cors from 'cors';
 
-export const client = redis.createClient(
-	{
-		host: process.env.REDIS_HOST,
-		port: process.env.REDIS_PORT,
-		password: process.env.REDIS_PASSWORD,
-	},
-);
+export const client = redis.createClient({
+	host: process.env.REDIS_HOST,
+	port: process.env.REDIS_PORT,
+	password: process.env.REDIS_PASSWORD,
+	tls: {},
+});
 
 export default nextConnect({
 	onError(err, req, res, next) {
@@ -17,4 +17,4 @@ export default nextConnect({
 	onNoMatch(req, res) {
 		return res.status(400).json({ success: false });
 	},
-});
+}).use(cors());
